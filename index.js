9e616e615ca7660b83b3d16e9622ea64c74828ed
Samantha Jordan <shinyguns@gmail.com>
2019-12-15 13:20:58 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var recipes = {
+  cake: "choco",
+  pie: "cherry"
+};
+
+var updateObjectWithKeyAndValue = function (object, key, value) {
+  var newObj = Object.assign({}, updateObjectWithKeyAndValue);
+    return newObj;
+};
