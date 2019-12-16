c929bda5d8d223104fb916ec5bd08753902a9c67
Lem Canady <lem.canady@gmail.com>
2019-12-16 12:33:12 -0800

    Update index.js
    Here's how I would do it.  Untested!

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ var updateObjectWithKeyAndValue = function (object, key, value) {
   var newObj = Object.assign({}, updateObjectWithKeyAndValue);
     return newObj;
 };
+
+var updateObjectWithKeyAndValue = (object, key, value) => Object.assign(object,{key:value})
