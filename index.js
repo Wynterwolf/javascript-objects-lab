52d40264d6aef415026ae15e507bd501f28bee8c
Samantha Jordan <shinyguns@gmail.com>
2019-12-16 23:04:30 -0600

    Update index.js
    added final two exercises

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ var recipes = {
   pie: "cherry"
 };
 
+//returns object with the original key value and new key value pair
+
 const updateObjectWithKeyAndValue = (object, key, value) => 
   Object.assign({}, object, { [key]: value });
   
 const destructivelyUpdateObjectWithKeyAndValue = (object, key, value) =>
-  Object.assign();
+  Object.assign(object, object, {[key]:value}, {[key]: value });
   
+  // also
+  
+  /* const destructivelyUpdateObjectWithKeyAndValue = (object, key, value) =>
+    Object.assign(object, {[key]: value }); */
+    
+  //   1) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
+  
+const deleteFromObjectByKey = (key,object) => {
+  Object.assign({}, object, {} , {});
+    delete object.key;
+  
+};
+
+//  2) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
+
+//   3) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
+
+
 const destructivelyDeleteFromObjectByKey = (object, key) => {};
